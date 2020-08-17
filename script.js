4d8e2a2fa8b40387be3e5d611998abaa0919e88f
AlaaEddin <a.e.i9444@gmail.com>
2020-08-18 02:11:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,29 +5,61 @@ let prevBtn = document.querySelector('.prevBtn');
 let nextBtn = document.querySelector('.nextBtn');
 let runBtn = document.querySelector('.run');
 let stopBtn = document.querySelector('.stop');
+let sliderNav = document.getElementById('navigator');
 
-// Function on click next button
-function nextSlide() {
-    // hide the currentSlide
-    slides[currentSlide].className = 'slide';
-    // Update currentSlide to be as Next Slide
-    currentSlide = (currentSlide + 1) % slides.length;
-    // Show the nextSlide
-    slides[currentSlide].className ='slide showSlide';
+// [=== Add dynamic dots for the sliderNav ===]
+for (i = 0; i < slides.length; i++) {
+    // Create span in the DOM
+    let dots = document.createElement('span');
+    // Append the created span inside the sliderNav
+    sliderNav.append(dots);
+    // Give the created spans a class
+    dots.className = 'navigator';
+
+    sliderNav.addEventListener('click', () => {
+        nextSlide();
+    });
 }
 
-// Function going to work on previous button click
-function prevSlide() {
+// [===When click on one of the dots we will go to the correct slide===]
+let sliderNavDots = document.querySelectorAll('#navigator .navigator');
+
+// Getting all dots in a forEach loop and detect the click was on which element
+sliderNavDots.forEach(function(dotElement, dotIndex){
+    dotElement.onclick = function() {
+        currentSlide = dotIndex;
+    }
+})
+
+// [=== To add the class currentDot to the first dot ===]
+sliderNavDots[currentSlide].classList.add('currentDot');
+
+// [=== Function on click next button ===]
+function nextSlide() {
+    // hide the currentSlide and remove currentDot class from the dot
     slides[currentSlide].className = 'slide';
+    sliderNavDots[currentSlide].className = 'navigator';
+    // Update currentSlide to be as Next Slide
+    currentSlide = (currentSlide + 1) % slides.length;
+    // Show the nextSlide and update which dot to make it active
+    slides[currentSlide].className ='slide showSlide';
+    sliderNavDots[currentSlide].className = 'navigator currentDot';
+}
+
+// [=== Function going to work on previous button click ===]
+function prevSlide() {
+    // hide the currentSlide and remove currentDot class from the dot
+    slides[currentSlide].className = 'slide';
+    sliderNavDots[currentSlide].className = 'navigator';
     // Update currentSlide value
     currentSlide = (currentSlide - 1) % slides.length;
     // Fix currentSlide when it's -1
     if (currentSlide === -1) {
         currentSlide = slides.length - 1;
     }
-    // Apply the updated value of currentSlide
+    // Apply the updated value of currentSlide and dots
     slides[currentSlide].className = 'slide showSlide';
-
+    sliderNavDots[currentSlide].className = 'navigator currentDot';
 }
 
 nextBtn.addEventListener('click', () => {
@@ -55,3 +87,17 @@ function runSlide() {
 runBtn.addEventListener('click', () => {
     runSlide();
 })
+
+// [Start] Function will take action based on where we apply mouseover and mouseout
+let sliderBtns = document.querySelectorAll('.slider-btns');
+let sliderImgs = document.querySelectorAll('.slide');
+let sliderItems = [...sliderBtns, ...sliderImgs, ...sliderNav.childNodes];
+
+function updateAutoSlidingStatus(elements, event, func) {
+    elements.forEach(element => {
+        element.addEventListener(event, func);
+    })
+}
+// Actions when mouseover and mouseout on and from next and prev btns, slides, dots
+updateAutoSlidingStatus(sliderItems, 'mouseover', stopSlide);
+updateAutoSlidingStatus(sliderItems, 'mouseout', runSlide);
